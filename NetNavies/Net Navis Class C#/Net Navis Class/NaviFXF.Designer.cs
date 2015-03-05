3dc8dc8025d64b0ecd0f9c833505b511afa3f077
Toad <Toad@localhost>
2015-03-05 02:41:39 -0800

    Changed To GLControl from a Custom GLContex

--- NetNavies/Net Navis Class C#/Net Navis Class/NaviFXF.Designer.cs
+++ NetNavies/Net Navis Class C#/Net Navis Class/NaviFXF.Designer.cs
@@ -34,20 +34,31 @@ namespace Net_Navis
 		[System.Diagnostics.DebuggerStepThrough()]
 		private void InitializeComponent()
 		{
+            this.glControl1 = new OpenTK.GLControl();
             this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Enabled = false;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(320, 200);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
             // 
             // NaviFXF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 247);
+            this.ClientSize = new System.Drawing.Size(320, 200);
+            this.Controls.Add(this.glControl1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "NaviFXF";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "NaviDX";
-            this.TopMost = true;
             this.ResumeLayout(false);
 
 		}
@@ -55,5 +66,7 @@ namespace Net_Navis
 		{
 			InitializeComponent();
         }
+
+        public OpenTK.GLControl glControl1;
 	}
 }
